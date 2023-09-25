f5d647d43657f9577280a582f4a7794dae6472c5
Maria Grazia <mariagraziadante@gmail.com>
2023-09-26 00:12:44 +0200

    Create CheckSpace.java
    Esercizio2- Tipi primitivi in Java

---
+++ CheckSpace.java
@@ -0,0 +1,9 @@
+public class CheckSpace 
+{
+    public static void main(String args[]) {
+        char caracter1 = 's';
+        char caracter2 = 'i';
+        String union = "" + caracter1 + caracter2;
+        System.out.println("The word is: " + union);
+    }
+}

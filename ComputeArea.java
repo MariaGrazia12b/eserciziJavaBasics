f662b19105168c29962d0243ddb6f97113cc481d
Maria Grazia <mariagraziadante@gmail.com>
2023-09-25 23:50:09 +0200

    Create ComputeArea.java
    Esercizio 1- Tipi primitivi in Java

---
+++ ComputeArea.java
@@ -0,0 +1,10 @@
+public class ComputeArea 
+{
+    public static void main(String args[]) 
+    {
+        int base = 30;
+        int high = 50;
+        int area = (base * high) / 2;
+        System.out.println("Area: " + area);
+    }
+}

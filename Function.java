383ee6d73487512a56092d1b4fe876c1ec3c14c4
Maria Grazia <mariagraziadante@gmail.com>
2023-09-26 13:11:13 +0200

    Create Function.java
    Esercizio- funzione che accetta in ingresso due interi e ne restituisce la somma

---
+++ Function.java
@@ -0,0 +1,13 @@
+public class Function 
+{
+    public static void main(String args[]){
+        int x = 9;
+        int y = 7;
+        System.out.println("Sum of x+y: " + sum(x,y)); 
+    }
+    public static int sum(int x, int y){
+        int z = x + y;
+        return z;
+    }
+}
+

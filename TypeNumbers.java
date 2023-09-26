23d9bdcb55365476fb66f9dc2e011ff111fa61a3
Maria Grazia <mariagraziadante@gmail.com>
2023-09-26 12:06:29 +0200

    Create TypeNumbers.java
    Esercizio- conversione tipi primitivi

---
+++ TypeNumbers.java
@@ -0,0 +1,8 @@
+public class TypeNumbers {
+    public static void main(String args[]) {
+        double number1 = 5.3;
+        double number2 = 3.8;
+        int number3 = (int)number1 + (int)number2;
+        System.out.println("The number3 is: " + number3);
+    }
+}

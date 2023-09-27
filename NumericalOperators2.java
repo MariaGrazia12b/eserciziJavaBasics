71ddc4d9b1e39d621d0af75047bce11c4791a216
Maria Grazia <mariagraziadante@gmail.com>
2023-09-27 11:45:50 +0200

    Create NumericalOperators2.java
    Esercizio 2 sugli operatori aritmetici

---
+++ NumericalOperators2.java
@@ -0,0 +1,15 @@
+// Definisci un metodo che calcoli la media tra 3 numeri e restituisca il risultato che dovrà essere stampato.
+
+public class NumericalOperators2 {
+    public static void main(String args[]) {
+        int number1 = 60;
+        int number2 = 34;
+        int number3 = 78;
+        System.out.println("Number1: " + number1);
+        System.out.println("Number2: " + number2);
+        System.out.println("Number3: " + number3);
+        
+        int result = (number1 + number2 + number3) / 2;
+        System.out.println("Arithmetic average: " + result);
+    }
+}

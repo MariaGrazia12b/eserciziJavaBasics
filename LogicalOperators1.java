84b3445162fd0636a94cbf9d4054965dc24bef32
Maria Grazia <mariagraziadante@gmail.com>
2023-09-27 17:09:13 +0200

    Create LogicalOperators1.java
    Esercizio 1 sugli operatori logici

---
+++ LogicalOperators1.java
@@ -0,0 +1,15 @@
+// Scrivi un programma che contiene un metodo che dati 2 interi in ingresso ed un valore di confronto
+// verifichi se il numero di confronto è compreso tra due valori specifici e stampi il risultato dell'operazione 
+// comprensivo dei due limiti (inferiore e superiore).
+
+public class LogicalOperators1 {
+    public static void main(String args[]) {
+        int x = 60;
+        int y = 39;
+        int z = 21;
+        System.out.println("Value of " + z + " is between " + x + " and " + y + "? " + comparison(x, y, z));
+    }
+    public static boolean comparison(int x, int y, int z) {
+        return (z < x && z < y);
+    }
+}

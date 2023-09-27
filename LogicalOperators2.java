885a48832954d760d39aa786b89029137104a236
Maria Grazia <mariagraziadante@gmail.com>
2023-09-27 23:20:44 +0200

    Create LogicalOperators2.java
    Esercizio 2 sugli operatori logici

---
+++ LogicalOperators2.java
@@ -0,0 +1,17 @@
+// Scrivi un programma che contiene un metodo che prenda un numero in ingresso e capisca se 
+// un numero è pari o dispari utilizzando solo gli operatori logici.
+
+public class LogicalOperators2 
+{
+    public static void main(String args[]) 
+    {
+        int x = 7;
+        System.out.println("x: " + x);
+        System.out.println("x è dispari? " + number(x));
+    }
+    public static boolean number(int x) 
+    {
+        boolean ris = (x % 2 != 0);
+        return ris;
+    }
+}

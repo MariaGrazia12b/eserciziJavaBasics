a2d88b43dbb12d7e5724e3ad3e3c4c4a7690ab6a
Maria Grazia <mariagraziadante@gmail.com>
2023-09-27 11:31:02 +0200

    Create NumericalOperators1.java
    Esercizio 1 sugli operatori aritmetici

---
+++ NumericalOperators1.java
@@ -0,0 +1,13 @@
+// Definisci un metodo per la divisione di 2 numeri interi che restituisca il quoziente che dovrà essere stampato.
+
+public class NumericalOperators1 {
+    public static void main(String args[]) {
+        int number1 = 90;
+        int number2 = 45;
+        System.out.println("Number1: " + number1);
+        System.out.println("Number2: " + number2);
+        
+        int resultDiv = number1 / number2;
+        System.out.println("Division: " + resultDiv);
+    }
+}

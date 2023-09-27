18e7e6d88708f2115f3e2025f942e23dd824fd96
Maria Grazia <mariagraziadante@gmail.com>
2023-09-27 14:44:27 +0200

    Create ComparisonOperators1.java
    Esercizio 1 sugli operatori di comparazione

---
+++ ComparisonOperators1.java
@@ -0,0 +1,14 @@
+// Scrivi un programma che contiene un metodo che confronti due numeri interi e determini se sono diversi.
+// Il programma dovrà stampare a video i due valori e il risultato dell'eguaglianza.
+
+public class ComparisonOperators1 {
+    public static void main(String args[]) {
+        int x = 68;
+        int y = 90;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        
+        boolean equals = x == y;
+        System.out.println("Equals x = y: " + equals);
+    }
+}

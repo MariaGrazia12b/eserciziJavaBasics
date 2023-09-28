d3ad6d8504495943d87abd603ff96468aeaacb3c
Maria Grazia <mariagraziadante@gmail.com>
2023-09-28 14:30:37 +0200

    Update IfElseStatements1.java
    AGGIUNTA COMMENTO

--- IfElseStatements1.java
+++ IfElseStatements1.java
@@ -1,6 +1,8 @@
 // Scrivere un programma che stampi i numeri da 1 a 100. Per i multipli di 3 stampi "Fizz" al posto del numero e per i 
 // multipli di 5 stampi "Buzz". Nel caso in cui un numero sia multiplo di entrambi stampi "FizzBuzz".
 
+// ESERCIZIO SVOLTO IN PAIR
+
  public class IfElseStatements1 {
      public static void main (String args[]) {
 

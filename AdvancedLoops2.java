8ed03a2498009880fe136c9349d9bb9930858a64
Maria Grazia <mariagraziadante@gmail.com>
2023-10-02 12:04:08 +0200

    Esercizio 2 advanced loops

---
+++ AdvancedLoops2.java
@@ -0,0 +1,19 @@
+// Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico e stampa tutti i numeri fino al valore immesso, 
+// la stampa dovrà saltare il valore uguale a 5.
+
+public class AdvancedLoops2 {
+    public static void main(String args[]) {
+        int x = 8;
+        loop(x);
+    }
+    public static void loop(int x) {
+        int i = 0;
+        for(i = 0; i <= x; i++) {
+            
+        if(i == 5) {
+            continue;
+        }
+        System.out.println(i);
+      }
+    }
+}

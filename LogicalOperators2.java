17047af446065961077240af5546c6a1fa731b97
Maria Grazia <mariagraziadante@gmail.com>
2023-10-03 12:50:48 +0200

    Correzione errori

--- LogicalOperators2.java
+++ LogicalOperators2.java
@@ -1,17 +1,22 @@
 // Scrivi un programma che contiene un metodo che prenda un numero in ingresso e capisca se 
 // un numero è pari o dispari utilizzando solo gli operatori logici.
 
-public class LogicalOperators2 
+public class LogicalOperators2
 {
-    public static void main(String args[]) 
+    public static void main(String args[])
     {
         int x = 7;
         System.out.println("x: " + x);
-        System.out.println("x è dispari? " + number(x));
+        System.out.println("x è dispari? " + isOdd(x));
+        System.out.println("x è pari? " + isEven(x));
     }
-    public static boolean number(int x) 
+    public static boolean isOdd(int x)
     {
         boolean ris = (x % 2 != 0);
         return ris;
+    }
+    public static boolean isEven(int x) {
+        boolean ris2 = (x % 2 == 0);
+        return ris2;
     }
 }

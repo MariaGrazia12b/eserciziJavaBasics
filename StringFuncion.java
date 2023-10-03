6543881c332a80ba3d9c1150d011b3e129b14f69
Maria Grazia <mariagraziadante@gmail.com>
2023-10-03 15:19:46 +0200

    Correzione errore

--- StringFuncion.java
+++ StringFuncion.java
@@ -3,8 +3,8 @@ public class StringFuncion {
         String word = "watermelon";
         System.out.println("Length: " + length(word));
     }
-    public static String length(String word){
+    public static int length(String word){
         int wordLength = word.length();
-        return String.valueOf(wordLength);
+        return wordLength;
     }
 }

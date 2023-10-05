5cdff0d6910fe93fe427f4254a212a184422c0d9
Maria Grazia <mariagraziadante@gmail.com>
2023-10-05 18:15:54 +0200

    Aggiunta se è rettangolo

--- project1/src/EsercizioPair.java
+++ project1/src/EsercizioPair.java
@@ -4,22 +4,50 @@ public class EsercizioPair {
         public static void main(String[] args) {
 
                 int[] uguale = {4, 4, 4};
-                int[] notATriangle = {2, 9, 2};
+                int[] notATriangle = {40, 2, 5};
                 int[] isoscele = {4, 6, 4};
+                int[] scaleno = {4, 2, 3};
 
-                verificaTriangolo(notATriangle);
+                verificaTriangolo(scaleno);
+        }
+        public static void verificaTriangolo(int[] myTriangle) {
+                Arrays.sort(myTriangle);
 
+                double power1 = Math.pow(myTriangle[0], 2);
+                double power2 = Math.pow(myTriangle[1], 2);
+                double power3 = Math.pow(myTriangle[2], 2);
+                // Calcolo delle potenze del valore dei lati per applicare il
+                // teorema di Pitagora.
+
+                if ((myTriangle[2] + myTriangle[1] > myTriangle[0])
+                        && (myTriangle[0] + myTriangle[1] > myTriangle[2])
+                        && (myTriangle[2] + myTriangle[0] > myTriangle[1])
+                        // Teorema di ineguaglianza del triangolo.
+                )
+                {
+                        System.out.println("La figura è un triangolo.");
+
+                        if (myTriangle[0] == myTriangle[1] && myTriangle[1] == myTriangle[2]) {
+                                System.out.println("E' un triangolo equilatero.");
+                        } else if (myTriangle[0] == myTriangle[1]) {
+                                System.out.println("E' un tringolo isoscele.");
+                                pitagora(power1, power2, power3);
+
+                        } else if (myTriangle[0] != myTriangle[1] && myTriangle[1] != myTriangle[2]) {
+                                System.out.println("E' un tringolo scaleno.");
+                                pitagora(power1, power2, power3);
+                        }
+
+                } else {
+                        System.out.println("La figura non è un triangolo.");
+                }
         }
 
-
-        public static void verificaTriangolo(int [] myTriangle) {
-                Arrays.sort(myTriangle);
-                if (myTriangle[0] == myTriangle[1] && myTriangle[1] == myTriangle[2]) {
-                        System.out.println("E' un triangolo equilatero.");
-                } else if ((myTriangle[0] + myTriangle[1]) <= myTriangle[2]) {
-                        System.out.println("Non si tratta di un triangolo.");
-                } else if (myTriangle[0] == myTriangle[1] && (myTriangle[0] + myTriangle[1]) <= myTriangle[2]) {
-                        System.out.println("E' un tringolo isoscele.");
+        public static void pitagora (double a, double b, double c) {
+                if ((a + b) == c) {
+                        System.out.println("Il triangolo è anche rettangolo.");
+                } else {
+                        System.out.println(("Non si tratta di un triangolo rettangolo."));
                 }
         }
 }

4957f2ba0c689db374b09d1171c8b4b0e0c9e075
Maria Grazia <mariagraziadante@gmail.com>
2023-10-24 16:23:48 +0200

    Verifica se un intero è nel range

---
+++ Exception e gestione degli errori 1/src/Main.java
@@ -0,0 +1,26 @@
+// Scrivere una funzione che controlli se un numero int è in un determinato range.
+// Se lo è ritorna true se non lo è lancia un'eccezione.
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 45;
+        int number1 = 90;
+
+        int min = 1;
+        int max = 50;
+        
+        System.out.println(number + " è nel range? " + verificaRange(number, min, max));
+        System.out.println(number1 + " è nel range? " + verificaRange(number1, min, max));
+    }
+    public static boolean verificaRange (int number, int min, int max) {
+        try {
+            if (number < min || number > max) {
+                throw new ArithmeticException ("Il numero non è nel range");
+            }
+            return true;
+        } catch (ArithmeticException e) {
+            System.out.println("C'è stato un errore " + e);
+            return false;
+        }
+    }
+}

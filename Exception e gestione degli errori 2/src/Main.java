3a9fff493d877a0a77c55b4911cb4ba89975924a
Maria Grazia <mariagraziadante@gmail.com>
2023-10-27 12:38:51 +0200

    Correzione

--- Exception e gestione degli errori 2/src/Main.java
+++ Exception e gestione degli errori 2/src/Main.java
@@ -1,23 +1,24 @@
 // Scrivere una funzione che controlli se un carattere è un numero altrimenti lanciare una eccezione.
 
+import java.nio.charset.CharacterCodingException;
+
 public class Main {
     public static void main(String[] args) {
         char character = 'i';
-        controlloCarattere(character);
+
+        try {
+            controlloCarattere(character);
+        } catch (ArithmeticException e) {
+            System.out.println("ERRORE: " + e);
+        }
     }
 
     public static void controlloCarattere (char character) {
-        try {
             // Character.isDigit() per verificare se il carattere è un numero.
             if (!Character.isDigit(character)) {
-                throw new ArithmeticException ();
+                throw new ArithmeticException ("Il carattere NON è un numero");
             } else {
                 System.out.println("Il carattere è un numero.");
-
             }
-        } catch (ArithmeticException e) {
-            System.out.println("C'è stato un errore " + e);
-            System.out.println(("Il carattere " + character + " non è un numero"));
-        }
     }
 }

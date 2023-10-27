c0002aac6c20a6ff3852994cc1d0cfd2c20012f0
Maria Grazia <mariagraziadante@gmail.com>
2023-10-27 12:25:26 +0200

    Correzione

--- Exception e gestione degli errori 1/src/Main.java
+++ Exception e gestione degli errori 1/src/Main.java
@@ -1,26 +1,27 @@
 // Scrivere una funzione che controlli se un numero int è in un determinato range.
 // Se lo è ritorna true se non lo è lancia un'eccezione.
 
+//Hai usato throw con try e catch insieme, throw ti serve per lanciare l'eccezione nel metodo,
+// il try e catch ti serve per gestire l'eccezione li dove è chiamato il metodo.
+
 public class Main {
     public static void main(String[] args) {
-        int number = 45;
-        int number1 = 90;
+        int number = 90;
 
         int min = 1;
         int max = 50;
 
-        System.out.println(number + " è nel range? " + verificaRange(number, min, max));
-        System.out.println(number1 + " è nel range? " + verificaRange(number1, min, max));
+        try {
+            System.out.println(number + " è nel range? " + verificaRange(number, min, max));
+        } catch (ArithmeticException e) {
+            System.out.println("ERRORE " + e);
+        }
     }
     public static boolean verificaRange (int number, int min, int max) {
-        try {
+
             if (number < min || number > max) {
                 throw new ArithmeticException ("Il numero non è nel range");
             }
             return true;
-        } catch (ArithmeticException e) {
-            System.out.println("C'è stato un errore " + e);
-            return false;
-        }
     }
 }

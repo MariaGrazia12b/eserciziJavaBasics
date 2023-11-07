16a4aefef249e0349af556e5ceb037c64c2c1619
Maria Grazia <mariagraziadante@gmail.com>
2023-11-07 13:28:19 +0100

    correzione con metodi

--- Date 5/src/Main.java
+++ Date 5/src/Main.java
@@ -19,25 +19,36 @@ public class Main {
         String data2Stringa = data2.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
         System.out.println("Data 2: " +data2Stringa);
 
-        //verificare che la prima data è precedente alla seconda
+        verificaPrecedenzaPrimaData(data1, data2);
+        verificaSuccessioneSecondaData(data1, data2);
+        leDueDateSonoUgualiAdOra(data1, data2);
+
+    }
+    public static boolean verificaPrecedenzaPrimaData (OffsetDateTime data1, OffsetDateTime data2) {
         if (data1.isBefore(data2)) {
             System.out.println("La prima data è precedente alla seconda");
+            return true;
         } else {
             System.out.println("La seconda data è precedente alla seconda");
         }
-
-        //verificare che la seconda data è successiva alla prima
+        return false;
+    }
+    public static boolean verificaSuccessioneSecondaData (OffsetDateTime data1, OffsetDateTime data2) {
         if (data2.isAfter(data1)) {
             System.out.println("La seconda data è successiva alla prima.");
+            return true;
         } else {
             System.out.println("La seconda data non è successiva alla prima.");
         }
-
-        //verificare che le due date sono uguali ad ora
+        return false;
+    }
+    public static boolean leDueDateSonoUgualiAdOra (OffsetDateTime data1, OffsetDateTime data2) {
         if (data1.isEqual(OffsetDateTime.now()) && data2.isEqual(OffsetDateTime.now())) {
             System.out.println("Le due date sono uguali alla data attuale");
+            return true;
         } else {
             System.out.println("Le due date non sono uguali alla data attuale");
         }
+        return false;
     }
 }

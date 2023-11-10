dc138304701cbb3f513a8244500403be6bd523be
Maria Grazia <mariagraziadante@gmail.com>
2023-11-10 23:07:50 +0100

    aggiunta test parsing

--- Test 3/src/Main.java
+++ Test 3/src/Main.java
@@ -3,29 +3,31 @@
 //Stampa sulla console
 //Crea dei test per questo esercizio
 
+import java.time.DateTimeException;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Locale;
+import java.util.zip.DataFormatException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DataFormatException {
         dataManuale("2023-03-01T13:00:00Z");
 
     }
-    public static String dataManuale (String data) {
+    public static String dataManuale (String data) throws DateTimeParseException, DataFormatException {
         if (data != null && ! data.isEmpty()) {
             try {
-                OffsetDateTime data1 = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+                OffsetDateTime data1 = OffsetDateTime.parse(data);
                 String dataStringManuale = data1.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
                 System.out.println("Data formattata manualmente: " +dataStringManuale);
                 return dataStringManuale;
-            }catch (Exception e) {
-            System.out.println("Errore nella formattazione della data: " + e.getMessage());
-            return null;
-        }
+            }catch (DateTimeParseException e) {
+                throw new DateTimeParseException("Errore di parsing", data, e.getErrorIndex());
+            }
     } else {
         System.out.println("Stringa data vuota o nulla");
-        return null;
+        throw new DataFormatException("Stringa data vuota o nulla");
         }
     }
 }
--- Test 3/src/MainTest.java
+++ Test 3/src/MainTest.java
@@ -1,24 +1,36 @@
 import org.junit.Test;
 
+import java.time.DateTimeException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.zip.DataFormatException;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
     private final Main test = new Main();
 
     @Test
-    public void data_manuale () {
+    public void data_manuale () throws DataFormatException {
         String result = test.dataManuale ("2023-03-01T13:00:00Z");
         assertEquals("01 marzo 2023", result);
     }
     @Test
-    public void data_null () {
-        String result = test.dataManuale (null);
-        assertNull(null, result);
+    public void data_null () throws DataFormatException {
+        String nonValida = null;
+        Exception e = assertThrows(DataFormatException.class, () -> test.dataManuale(nonValida));
+        assertEquals("Stringa data vuota o nulla", e.getMessage());
     }
     @Test
-    public void data_empty () {
-        String result = test.dataManuale ("");
-        assertNull(null, result);
+    public void data_empty () throws DataFormatException {
+        String nonValida = "";
+        Exception e = assertThrows(DataFormatException.class, () -> test.dataManuale(nonValida));
+        assertEquals("Stringa data vuota o nulla", e.getMessage());
     }
-
+    @Test
+    public void data_parsing () {
+        String nonValida = "data_non_valida";
+        Exception e = assertThrows(DateTimeParseException.class, () -> test.dataManuale(nonValida));
+        assertEquals("Errore di parsing", e.getMessage());
+    }
 }

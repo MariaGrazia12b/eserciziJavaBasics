797d0a12ba642187ca4592149a363b694bcb0f27
Maria Grazia <mariagraziadante@gmail.com>
2024-02-01 00:13:45 +0100

    correzione pathvariable

--- demo-exercise1/src/main/java/com/exercise/demoexercise1/controller/DemoExcercise2Controller.java
+++ demo-exercise1/src/main/java/com/exercise/demoexercise1/controller/DemoExcercise2Controller.java
@@ -1,17 +1,15 @@
 package com.exercise.demoexercise1.controller;
 
 import com.exercise.demoexercise1.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v2")
 public class DemoExcercise2Controller
 {
     @GetMapping(path = "/ciao")
-    public User ciao (@RequestParam String nome, @RequestParam String provincia) {
+    public User ciao (@RequestParam String nome, @PathVariable String provincia) {
         return new User(nome, provincia);
     }
 }
--- demo-exercise1/src/main/java/com/exercise/demoexercise1/controller/DemoExercise1Controller.java
+++ demo-exercise1/src/main/java/com/exercise/demoexercise1/controller/DemoExercise1Controller.java
@@ -1,17 +1,14 @@
 package com.exercise.demoexercise1.controller;
 
 import com.exercise.demoexercise1.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1")
 
 public class DemoExercise1Controller {
     @GetMapping(path = "/ciao1")
-    public String tempoLombardia (@RequestParam String nome, @RequestParam String provincia) {
+    public String tempoLombardia (@RequestParam String nome, @PathVariable String provincia) {
         return "Ciao " + nome + ", com'è il tempo in " + provincia + "?";
     }
 
